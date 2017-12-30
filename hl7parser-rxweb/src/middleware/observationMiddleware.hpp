7228864f5aeb2bc16cdc1ef2b92388f84c183ef9
once-ler <htao@eztier.com>
2017-12-30 12:13:36 -0500

    Add custom observation middleware

--- hl7parser-rxweb/src/middleware/observationMiddleware.hpp
+++ hl7parser-rxweb/src/middleware/observationMiddleware.hpp
@@ -25,8 +25,28 @@ namespace hl7parserrxweb::middleware {
       [&isADT](const rxweb::task<S>& t) { return regex_match(t.type, isMDM); },
       [&](const rxweb::task<S>& t) {
         // Iterate through all OBX and lookup for positive responses.
+        HL7MessageModel o = *(t.data);
+        HL7Message hl7m{o.message};
+        auto observations = hl7m.observations();
+        bool persist = false;
+        for(const auto o : observations) {
+          auto vals = o->ObservationValues();     
+          // Find "Y" in list of observations.
+          regex ex("y", regex_constants::icase | regex_constants::ECMAScript);
+          auto it = find_if(vals.begin(), vals.end(), [&ex](const auto& m)->bool { return std::regex_match(m, ex); });
 
-        // Persist patient object to SQL.
+          if (it != vals.end()) {
+            cout << *it << endl;
+            persist = true;
+            break;
+          }
+        }
+        
+        // Persist patient object to SQL if any response is "Y".
+        if (persist) {
+
+        }
+
       }
     };
   }
