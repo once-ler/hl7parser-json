6103716e528f57ddf93409b74626e6d72ecc3914
once-ler <htao@eztier.com>
2020-11-27 22:35:59 -0500

    Add helper functions for implicit json conversion for typed PID segment fields

--- hl7parser-json/model/PID/PatientAddress.hpp
+++ hl7parser-json/model/PID/PatientAddress.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <iostream>
+#include "json.hpp"
 
 using namespace std;
+
+using json = nlohmann::json;
 
 namespace hl7parserJson::model::PID {
   struct PatientAddress {
@@ -19,4 +22,36 @@ namespace hl7parserJson::model::PID {
     string _11_AddressRepresentationCode;
     string _12_AddressValidityRange;
   };
+
+  void to_json(json& j, const PatientAddress& p) {
+    j = json{
+      { "_1_StreetAddress", p._1_StreetAddress },
+      { "_2_OtherDesignation", p._2_OtherDesignation },
+      { "_3_City", p._3_City },
+      { "_4_StateOrProvince", p._4_StateOrProvince },
+      { "_5_ZipOrPostalCode", p._5_ZipOrPostalCode },
+      { "_6_Country", p._6_Country },
+      { "_7_AddressType", p._7_AddressType },
+      { "_8_OtherGeographicDesignation", p._8_OtherGeographicDesignation },
+      { "_9_CountyParishCode", p._9_CountyParishCode },
+      { "_10_CensusTract", p._10_CensusTract },
+      { "_11_AddressRepresentationCode", p._11_AddressRepresentationCode },
+      { "_12_AddressValidityRange", p._12_AddressValidityRange }
+    };
+  }
+
+  void from_json(const json& j, PatientAddress& p) {
+    p._1_StreetAddress = j.value("_1_StreetAddress", "");
+    p._2_OtherDesignation = j.value("_2_OtherDesignation", "");
+    p._3_City = j.value("_3_City", "");
+    p._4_StateOrProvince = j.value("_4_StateOrProvince", "");
+    p._5_ZipOrPostalCode = j.value("_5_ZipOrPostalCode", "");
+    p._6_Country = j.value("_6_Country", "");
+    p._7_AddressType = j.value("_7_AddressType", "");
+    p._8_OtherGeographicDesignation = j.value("_8_OtherGeographicDesignation", "");
+    p._9_CountyParishCode = j.value("_9_CountyParishCode", "");
+    p._10_CensusTract = j.value("_10_CensusTract", "");
+    p._11_AddressRepresentationCode = j.value("_11_AddressRepresentationCode", "");
+    p._12_AddressValidityRange = j.value("_12_AddressValidityRange", "");
+  }
 }
--- hl7parser-json/model/PID/PhoneNumberHome.hpp
+++ hl7parser-json/model/PID/PhoneNumberHome.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <iostream>
+#include "json.hpp"
 
 using namespace std;
+
+using json = nlohmann::json;
 
 namespace hl7parserJson::model::PID {
 
@@ -18,4 +21,29 @@ namespace hl7parserJson::model::PID {
     string _9_AnyText;
   };
 
+  void to_json(json& j, const PhoneNumberHome& p) {
+    j = json{
+      { "_1_TelephoneNumber", p._1_TelephoneNumber },
+      { "_2_TelecommunicationUseCode", p._2_TelecommunicationUseCode },
+      { "_3_TelecommunicationEquipmentType", p._3_TelecommunicationEquipmentType },
+      { "_4_EmailAddress", p._4_EmailAddress },
+      { "_5_CountryCode", p._5_CountryCode },
+      { "_6_AreaCityCode", p._6_AreaCityCode },
+      { "_7_PhoneNumber", p._7_PhoneNumber },
+      { "_8_Extension", p._8_Extension },
+      { "_9_AnyText", p._9_AnyText }
+    };
+  }
+
+  void from_json(const json& j, PhoneNumberHome& p) {
+    p._1_TelephoneNumber = j.value("_1_TelephoneNumber", "");
+    p._2_TelecommunicationUseCode = j.value("_2_TelecommunicationUseCode", "");
+    p._3_TelecommunicationEquipmentType = j.value("_3_TelecommunicationEquipmentType", "");
+    p._4_EmailAddress = j.value("_4_EmailAddress", "");
+    p._5_CountryCode = j.value("_5_CountryCode", "");
+    p._6_AreaCityCode = j.value("_6_AreaCityCode", "");
+    p._7_PhoneNumber = j.value("_7_PhoneNumber", "");
+    p._8_Extension = j.value("_8_Extension", "");
+    p._9_AnyText = j.value("_9_AnyText", "");
+  }
 }
